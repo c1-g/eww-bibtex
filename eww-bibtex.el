b65dfdd0a8a8c9f952fd27a9dec137498dd0af24
c1-g <char1iegordon@protonmail.com>
2021-11-25 16:34:16 +0700

    Make eww-bibtex takes html-source string as argument too.

--- eww-bibtex.el
+++ eww-bibtex.el
@@ -5,6 +5,7 @@
 (require 'esxml)
 (require 'eww)
 (require 'dash)
+(require 'dom)
 
 (defcustom eww-bibtex-default-bibliography bibtex-files
   "List of BibTeX files that `eww-bibtex' will append new entry to.
@@ -15,78 +16,106 @@ The `eww-bibtex' command will have user select only one element from
 this list. If an element is a directory, the command will have user
 select all BibTeX files in it.")
 
-(defcustom eww-bibtex-finalize-field-list-functions
-  (list #'eww-bibtex-find-author-for-wikipedia
-        #'identity)
-  "")
+(defvar eww-bibtex-build-field-list-functions
+  (list
+   #'eww-bibtex-get-author
+   #'eww-bibtex-get-title
+   #'eww-bibtex-get-year
+   #'eww-bibtex-get-url
+   #'eww-bibtex-get-note))
+
+(defcustom eww-bibtex-modify-field-list-functions
+  (list
+   #'eww-bibtex-find-author-for-wikipedia)
+  ""
+  :type 'hook)
 
 (defcustom eww-bibtex-find-ref-key-functions
   (list #'eww-bibtex-find-key-for-wikipedia)
   ""
   :type 'hook)
 
-(defun eww-bibtex-update-field-alist ()
-  (dolist (elt eww-bibtex-field-alist)
-    ;; Entry commands
-    (let* ((field-name (car elt))
-           (field (cdr elt))
-           (fname (intern (format "eww-bibtex-get-%s" field-name))))
-      (cond ((symbolp field)
-             (defalias fname
-               (symbol-function field)))
-            ((or (eq 'lambda (car field))
-                 (eq 'function (car field)))
-             (defalias fname
-               field
-               "Alias for an anonymous function defined by `eww-bibtex-field-alist'"))
-            (t (defalias fname
-                 (lambda ()
-                   (:documentation
-                    "Automatically defined by `eww-bibtex-update-field-alist'.")
-                   (interactive)
-                   (eww-bibtex--query field-name t))))))))
+(defun eww-bibtex-build-field-list (field-list dom func-list)
+  (cond ((null func-list) field-list)
+        (t (eww-bibtex-build-field-list
+            (if (string-prefix-p "eww-bibtex-get-" (symbol-name (car func-list)))
+                 (let* ((field-name (string-remove-prefix "eww-bibtex-get-"
+                                                          (symbol-name (car func-list))))
+                        (value-list (-list (funcall (car func-list) field-list dom)))
+                        (value (if (> (length value-list) 1)
+                                   (completing-read (concat "Which " field-name "? ") value-list)
+                                 (car value-list)))
+                        (field-elt (--find (equal (car it) field-name) field-list)))
+                   (if field-elt
+                       (-replace field-elt `(,field-name nil ,value) field-list)
+                     (push `(,field-name nil ,value) field-list))
+                   )
+               (-list (funcall (car func-list) field-list dom)))
+             dom
+            (cdr func-list)))))
 
-(defcustom eww-bibtex-field-alist
-  '(("author" . ("meta[name=author]"
-                 "meta[name=citation_author]"
-                 "[rel=author] > [itemprop=name]"
-                 "[rel=author]"
-                 "[itemprop=author] > *"
-                 "[itemprop=author]"
-                 ".author"))
-    ("title" . (lambda nil (plist-get eww-data :title)))
-    ("url" . ("link[rel=canonical]"
-              eww-current-url
-              "meta[name=citation_fulltext_html_url]"))
-    ("year" . ("meta[name=citation_publication_date]"
-               "meta[name=date]"
-               "[itemprop=dateModified]"
-               "[itemprop=datePublished]"
-               "[itemprop=dateModified]"
-               "[itemprop=datePublished]"
-               "[id*=updated]"
-               "time[pubdate]"
-               ".post_date"
-               "time"
-               (lambda nil (format-time-string "%Y"))))
-    ("note" . (lambda nil (format "[Online; accessed %s]" (format-time-string "%F")))))
-  ""
-  :type 'alist
-  :set (lambda (sym val)
-         (set-default sym val)
-         (eww-bibtex-update-field-alist)))
+(defun eww-bibtex-modify-field-list (field-list dom func-list)
+  (cond ((null func-list) field-list)
+        (t (let ((modified-field-list (funcall (car func-list) field-list dom)))
+             (eww-bibtex-modify-field-list
+              (or modified-field-list field-list)
+              dom
+              (cdr func-list))))))
 
-(defun eww-bibtex-find-key-for-wikipedia (fields)
-  (let ((url (car (last (assoc "url" fields)))))
+(defun eww-bibtex-get-author (field-list dom)
+  (eww-bibtex--query dom
+                     '("meta[name=author]"
+                       "meta[name=citation_author]"
+                       "[rel=author] > [itemprop=name]"
+                       "[rel=author]"
+                       "[itemprop=author] > *"
+                       "[itemprop=author]"
+                       ".author")))
+
+(defun eww-bibtex-get-title (field-list dom)
+  (let ((title (cl-caddr
+                 (car (dom-by-tag dom 'title)))))
+    (when title
+      (->>
+       title
+       (s-replace "\n" " ")
+       (s-trim)
+       (s-collapse-whitespace)))))
+
+(defun eww-bibtex-get-url (field-list dom)
+  (append (eww-bibtex--query dom '("link[rel=canonical]"
+                                      "meta[name=citation_fulltext_html_url]"))
+          (list (eww-current-url))))
+
+(defun eww-bibtex-get-year (field-list dom)
+  (append (eww-bibtex--query dom
+                             '("meta[name=citation_publication_date]"
+                               "meta[name=date]"
+                               "[itemprop=dateModified]"
+                               "[itemprop=datePublished]"
+                               "[itemprop=dateModified]"
+                               "[itemprop=datePublished]"
+                               "[id*=updated]"
+                               "time[pubdate]"
+                               ".post_date"
+                               "time"))
+          (list (format-time-string "%Y"))))
+
+(defun eww-bibtex-get-note (field-list _dom)
+  (format "[Online; accessed %s]" (format-time-string "%F")))
+
+(defun eww-bibtex-find-key-for-wikipedia (field-list)
+  (let ((url (car (last (assoc "url" field-list)))))
     (if (string-match-p "wikipedia" url)
         (concat "wiki:" (file-name-base url)))))
 
-(defun eww-bibtex-find-author-for-wikipedia (fields)
-  (when-let ((url (car (last (assoc "url" fields)))))
+(defun eww-bibtex-find-author-for-wikipedia (field-list _dom)
+  (when-let ((url (car (last (assoc "url" field-list)))))
     (if (string-match-p "wikipedia" url)
-        (-replace '("author" nil nil) '("author" nil "{Wikipedia Contributors}") fields))))
+        (-replace '("author" nil nil) '("author" nil "{Wikipedia Contributors}") field-list))))
 
-(defun eww-bibtex ()
+;;;###autoload
+(defun eww-bibtex (html-source &optional interactive)
   "Alter the \"Misc\" entry in `bibtex-BibTeX-entry-alist' based on
 `eww-bibtex-field-alist'.
 
@@ -120,38 +149,35 @@ the initial content of the field, based on `eww-bibtex-field-alist'.
 Take the \"note\" field, then replace its INIT argument with the result
 of `eww-bibtex-get-note' created automatically by
 `eww-bibtex-update-field-alist'."
-  (interactive nil eww-mode)
+  (interactive (list (plist-get eww-data :source) t) eww-mode)
   (let* ((misc-entry (assoc "Misc" bibtex-BibTeX-entry-alist))
-         (field-list (cl-fifth misc-entry))
-         (new-field-list)
-         (target-files (car (cl-loop
-                             for file in eww-bibtex-default-bibliography
-                             if (file-directory-p file)
-                             collect (directory-files-recursively file ".bib")
-                             else
-                             collect file))))
-    (setq new-field-list (cl-loop
-                          for elt in-ref eww-bibtex-field-alist
-                          collect
-                          (let* ((field-name (car elt))
-                                 (get-field-fn (intern (format "eww-bibtex-get-%s" field-name)))
-                                 (field (assoc field-name field-list)))
-                            (if field
-                                (-replace-at 2 (funcall get-field-fn)
-                                             (if (null (cdr field))
-                                                 (append field '(nil nil))
-                                               field))
-                              (list field-name nil (funcall get-field-fn))))))
-
+         (dom (with-temp-buffer
+                (insert html-source)
+                (libxml-parse-html-region
+                 (point-min)
+                 (point-max))))
+         (field-list (eww-bibtex-build-field-list
+                               (cl-fifth misc-entry)
+                               dom
+                               eww-bibtex-build-field-list-functions))
+         (target-files (flatten-tree (--map
+                                      (if (file-directory-p it)
+                                          (directory-files-recursively it ".bib")
+                                        it)
+                                      eww-bibtex-default-bibliography))))
     (setq misc-entry (-replace-at
                       4
-                      (run-hook-with-args-until-success 'eww-bibtex-finalize-field-list-functions new-field-list)
+                      (eww-bibtex-modify-field-list
+                       field-list
+                       dom
+                       eww-bibtex-modify-field-list-functions)
                       misc-entry))
     
     (pop-to-buffer (find-file-noselect (completing-read "Which BibTeX file? " target-files nil t)))
     
     (let ((bibtex-entry-alist (list misc-entry))
-          (bibtex-autokey-before-presentation-function (apply-partially #'eww-bibtex-replace-autokey new-field-list)))
+          (bibtex-autokey-before-presentation-function (apply-partially #'eww-bibtex-replace-autokey field-list)))
+      
       (bibtex-entry "Misc")
       (when (yes-or-no-p "Is the entry correct? ")
         (bibtex-clean-entry)))))
@@ -160,31 +186,17 @@ of `eww-bibtex-get-note' created automatically by
   (or (run-hook-with-args-until-success 'eww-bibtex-find-ref-key-functions field-list)
       autokey))
 
-(defun eww-bibtex--query (field &optional interactive)
-  (interactive
-   (list (completing-read "Which field?" (mapcar #'car eww-bibtex-field-alist))
-         t))
-  (when-let* ((selectors (if (eq (cadr (assoc field eww-bibtex-field-alist)) 'lambda)
-                             (list (cdr (assoc field eww-bibtex-field-alist)))
-                           (cdr (assoc field eww-bibtex-field-alist))))
-              (eww-source (plist-get eww-data :source))
-              (eww-dom (with-temp-buffer
-                         (insert eww-source)
-                         (libxml-parse-html-region (point-min) (point-max))))
-              (value-list (seq-filter
-                           #'stringp
-                           (flatten-tree
-                            (mapcar #'(lambda (selector)
-                                        (cond ((stringp selector)
-                                               (esxml-query-all selector eww-dom))
-                                              (t
-                                               (funcall selector))))
-                                    selectors)))))
-    (if (and interactive (> (length value-list) 1))
-         (completing-read (format "Which %s?" field) value-list)
-      (if interactive
-          (message (car value-list))
-        (car value-list)))))
-
+(defun eww-bibtex--query (dom selectors)
+  (flatten-list (mapcar (lambda (selector)
+                          (-difference
+                           (--> selector
+                                (esxml-query-all it dom)
+                                (flatten-tree it)
+                                (-filter #'stringp it))
+                           (--> selector
+                                (esxml-parse-css-selector selector)
+                                (flatten-tree it)
+                                (-filter #'stringp it))))
+                        selectors)))
 (provide 'eww-bibtex)
 ;;; eww-bibtex.el ends here
